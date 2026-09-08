6bebdc90f14dc2c2e642a49c7767ca1b68fee893
sammeksovitkar <83156211+sammeksovitkar@users.noreply.github.com>
2026-09-08 17:27:01 +0530

    Update App.js

--- board/src/App.js
+++ board/src/App.js
@@ -140,7 +140,7 @@ export default function App() {
     try {
       // 📌 Vercel / Local backend URL:
       const BACKEND_URL = process.env.NODE_ENV === 'production' 
-        ? 'https://backend-6c7bsb4qh-sammek-sovitkars-projects.vercel.app/api/upload-board' 
+        ? 'https://backend-gules-ten-84.vercel.app/' 
         : 'http://localhost:5000/api/upload-board';
 
       const response = await fetch(BACKEND_URL, {
